1321117f60de9809347807caf65c68f967429f8b
Nikola Nikic <nikola.nikic1994@gmail.com>
2021-12-08 20:40:20 +0100

    rock paper scissors complete

---
+++ js/script.js
@@ -0,0 +1,251 @@
+let rulesBtn = document.getElementById('rules');
+let rulesModal = document.getElementById('rulesModal');
+let closeModal = document.getElementById('close-modal');
+let modalContent = document.querySelector('.modal-content');
+
+// OPEN MODAL
+rulesBtn.addEventListener('click', () => {
+    rulesModal.style.display = 'block';
+    modalContent.style.animation = 'scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both';
+});
+
+// CLOSE MODAL
+closeModal.addEventListener('click', () => {
+    setTimeout(() => {
+        rulesModal.style.display = 'none';
+    }, 500)
+    
+    modalContent.style.animation = 'scale-out-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both';
+});
+
+
+// CHOICE VARIABLES
+let startGame = document.querySelector('.start-game');
+let endGame = document.querySelector('.end-game');
+let youPicked = document.querySelector('.player-choice');
+let computerPicked = document.querySelector('.computer-choice');
+let player = document.querySelector('#player');
+let computer = document.querySelector('#computer');
+let resultText = document.querySelector('#result-text');
+let scoreNumber = document.querySelector('#score-number');
+
+// LOCAL STORAGE
+let countWins = () => {
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+      } else {
+        localStorage.clickcount = 1;
+      }
+      scoreNumber.innerHTML = localStorage.clickcount;
+    } else {
+      scoreNumber.innerHTML = "Sorry, your browser does not support web storage...";
+    }
+  }
+
+function rememberCount() {
+    let storedNumber = localStorage.getItem('clickcount');
+    let placeholder = 0;
+
+    if (storedNumber === null) {
+        scoreNumber.innerHTML = placeholder;
+    } else {
+        scoreNumber.innerHTML = storedNumber;
+    }
+}
+
+// CHOOSE ROCK
+function chooseRock() {
+    endGame.style.display = 'flex';
+    startGame.style.display = 'none';
+
+    youPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 40">
+    <path fill="#3B4262" d="M45.06 12.22c-.642-8.096-9.734-7.269-9.734-7.269-3.837-6.765-9.832-1.865-9.832-1.865-4.606-6.63-10.38-.486-10.38-.486-9.957-1.074-9.571 7.066-9.571 7.066-.234 2.588 1.403 10.593 1.403 10.593-1.477-4.614-4.68-.784-4.68-.784-3.94 6.078-.975 9.405-.975 9.405 5.33 6.246 16.688 13.743 16.688 13.743 4.113 2.356 2.373 4.457 2.373 4.457l24.876-4.11.571-4.718c3.782-11.436-.739-26.032-.739-26.032z"/></svg>`;
+
+    player.classList.add('rock-bg');
+    player.classList.remove('scissors-bg');
+    player.classList.remove('paper-bg');
+
+    let computerRandom = Math.random().toFixed(2);
+    let computersChoice = '';
+
+    if (computerRandom > 0 && computerRandom < 0.33) {
+        computersChoice = 'rock';
+        computerPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 40">
+        <path fill="#3B4262" d="M45.06 12.22c-.642-8.096-9.734-7.269-9.734-7.269-3.837-6.765-9.832-1.865-9.832-1.865-4.606-6.63-10.38-.486-10.38-.486-9.957-1.074-9.571 7.066-9.571 7.066-.234 2.588 1.403 10.593 1.403 10.593-1.477-4.614-4.68-.784-4.68-.784-3.94 6.078-.975 9.405-.975 9.405 5.33 6.246 16.688 13.743 16.688 13.743 4.113 2.356 2.373 4.457 2.373 4.457l24.876-4.11.571-4.718c3.782-11.436-.739-26.032-.739-26.032z"/></svg>`;
+
+        computer.classList.add('rock-bg');
+        computer.classList.remove('scissors-bg');
+        computer.classList.remove('paper-bg');
+    }
+    if (computerRandom >= 0.33 && computerRandom < 0.66) {
+        computersChoice = 'paper';
+        computerPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 55">
+        <path fill="#3B4262" d="M47.125 11.832a2.922 2.922 0 00-1.232-.198c-.57.04-1.029.271-1.302.65-1.604 2.248-2.919 6.493-3.979 9.905-.486 1.577-1.14 3.688-1.612 4.69-.493-2.807.064-13.09.28-17.05l.003-.064c.15-2.751.17-3.234.138-3.446-.238-1.509-.843-2.5-1.799-2.943-.966-.45-2.22-.25-3.572.563-.677.41-.865 1.816-1.446 8.19l-.002.028c-.32 3.502-1.058 11.566-1.965 12.91-1.023-1.88-2.431-12.555-3.039-17.176-.425-3.236-.673-5.094-.84-5.655-.35-1.176-1.83-2.176-3.295-2.232-1.22-.06-2.22.56-2.698 1.638-.894.995-.578 4.292.41 12.102.47 3.718 1.44 11.395.83 12.257-1.219-.133-3.31-4.942-6.215-14.299-.816-2.62-1.068-3.408-1.318-3.753-.494-1.202-2.172-2.129-3.676-2.024a3.183 3.183 0 00-.377.049c-.787.156-2.584.881-2.2 4.226 1.06 4.637 2.213 8.041 3.331 11.346l.023.066c.669 1.98 1.302 3.85 1.89 5.925 1.385 4.9.846 7.94.84 7.975-.046.312-.143.503-.288.57a.556.556 0 01-.195.045c-.44.03-1.098-.26-1.437-.45-.776-1.482-4.636-8.544-8.134-9.524l-.126-.037-.127.012c-1.283.121-2.226.606-2.803 1.441-.914 1.32-.535 3.002-.444 3.34l.052.12c.028.051 2.834 5.165 3.268 7.544.374 2.04 2.311 4.25 3.869 6.026l.064.073c.508.58.946 1.083 1.292 1.548 4.519 4.713 11.665 8.677 11.723 8.71.892.657 1.387 1.293 1.44 1.84a.798.798 0 01-.16.58l-.155.162.988.96 18.853-1.324.804-3.684c2.486-10.402 1.967-19.272 1.958-19.33.01-.327.706-3.483 1.266-6.033l.017-.065c1.117-5.08 2.505-11.4 2.772-13.803.116-1.028-.542-1.972-1.675-2.401z"/></svg>`;
+
+        computer.classList.add('paper-bg');
+        computer.classList.remove('rock-bg');
+        computer.classList.remove('scissors-bg');
+    }
+    if (computerRandom >= 0.66 && computerRandom < 1) {
+        computersChoice = 'scissors';
+        computerPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 50">
+        <path fill="#3B4262" d="M13.971 25.702l6.012-8.415c-2.499-.415-7.088-.507-10.846 3.235C3.212 26.421.812 39.163.312 42.248L15.37 57.24c2.711-.232 14.713-1.827 26.279-13.34.122-.249 2.94-2.321.636-4.614-1.1-1.095-2.919-1.074-4.042.044-.572.57-1.461.577-2.021.02-.56-.557-.552-1.443.02-2.012l4.087-4.069c2.076-2.067.119-5.555-2.78-4.717l-3.345 2.851c-.611.53-1.52.439-2.022-.14-.519-.597-.408-1.503.183-2.013 11.687-10.208 9.98-8.979 17.5-15.995 2.809-2.329-.725-6.447-3.493-4.09L28.182 25.45c-.529.448-1.34.457-1.86-.02-.601-.517-.615-1.262-.222-1.85L38.787 3.944c1.854-2.5-1.795-5.277-3.749-2.757L16.28 27.307c-.452.65-1.364.8-1.985.345a1.377 1.377 0 01-.323-1.95z"/></svg>`;
+
+        computer.classList.add('scissors-bg');
+        computer.classList.remove('paper-bg');
+        computer.classList.remove('rock-bg');
+    }
+
+    console.log(computersChoice);
+
+    if (computersChoice == 'scissors') {
+        resultText.innerHTML = 'You win!';
+        player.classList.add('show-winner');
+        computer.classList.remove('show-winner');
+        countWins();
+    }
+    if (computersChoice == 'paper') {
+        resultText.innerHTML = 'You lose!';
+        player.classList.remove('show-winner');
+        computer.classList.add('show-winner');
+    }
+    if (computersChoice == 'rock') {
+        resultText.innerHTML = 'Draw!';
+        player.classList.remove('show-winner');
+        computer.classList.remove('show-winner');
+    }
+
+}
+
+// CHOOSE SCRISSORS
+function chooseScissors() {
+    endGame.style.display = 'flex';
+    startGame.style.display = 'none';
+
+    youPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 50">
+    <path fill="#3B4262" d="M13.971 25.702l6.012-8.415c-2.499-.415-7.088-.507-10.846 3.235C3.212 26.421.812 39.163.312 42.248L15.37 57.24c2.711-.232 14.713-1.827 26.279-13.34.122-.249 2.94-2.321.636-4.614-1.1-1.095-2.919-1.074-4.042.044-.572.57-1.461.577-2.021.02-.56-.557-.552-1.443.02-2.012l4.087-4.069c2.076-2.067.119-5.555-2.78-4.717l-3.345 2.851c-.611.53-1.52.439-2.022-.14-.519-.597-.408-1.503.183-2.013 11.687-10.208 9.98-8.979 17.5-15.995 2.809-2.329-.725-6.447-3.493-4.09L28.182 25.45c-.529.448-1.34.457-1.86-.02-.601-.517-.615-1.262-.222-1.85L38.787 3.944c1.854-2.5-1.795-5.277-3.749-2.757L16.28 27.307c-.452.65-1.364.8-1.985.345a1.377 1.377 0 01-.323-1.95z"/></svg>`
+
+    player.classList.add('scissors-bg');
+    player.classList.remove('paper-bg');
+    player.classList.remove('rock-bg');
+
+    let computerRandom = Math.random().toFixed(2);
+    let computersChoice = '';
+
+    if (computerRandom > 0 && computerRandom < 0.33) {
+        computersChoice = 'rock';
+        computerPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 40">
+        <path fill="#3B4262" d="M45.06 12.22c-.642-8.096-9.734-7.269-9.734-7.269-3.837-6.765-9.832-1.865-9.832-1.865-4.606-6.63-10.38-.486-10.38-.486-9.957-1.074-9.571 7.066-9.571 7.066-.234 2.588 1.403 10.593 1.403 10.593-1.477-4.614-4.68-.784-4.68-.784-3.94 6.078-.975 9.405-.975 9.405 5.33 6.246 16.688 13.743 16.688 13.743 4.113 2.356 2.373 4.457 2.373 4.457l24.876-4.11.571-4.718c3.782-11.436-.739-26.032-.739-26.032z"/></svg>`;
+
+        computer.classList.add('rock-bg');
+        computer.classList.remove('scissors-bg');
+        computer.classList.remove('paper-bg');
+    }
+    if (computerRandom >= 0.33 && computerRandom < 0.66) {
+        computersChoice = 'paper';
+        computerPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 55">
+        <path fill="#3B4262" d="M47.125 11.832a2.922 2.922 0 00-1.232-.198c-.57.04-1.029.271-1.302.65-1.604 2.248-2.919 6.493-3.979 9.905-.486 1.577-1.14 3.688-1.612 4.69-.493-2.807.064-13.09.28-17.05l.003-.064c.15-2.751.17-3.234.138-3.446-.238-1.509-.843-2.5-1.799-2.943-.966-.45-2.22-.25-3.572.563-.677.41-.865 1.816-1.446 8.19l-.002.028c-.32 3.502-1.058 11.566-1.965 12.91-1.023-1.88-2.431-12.555-3.039-17.176-.425-3.236-.673-5.094-.84-5.655-.35-1.176-1.83-2.176-3.295-2.232-1.22-.06-2.22.56-2.698 1.638-.894.995-.578 4.292.41 12.102.47 3.718 1.44 11.395.83 12.257-1.219-.133-3.31-4.942-6.215-14.299-.816-2.62-1.068-3.408-1.318-3.753-.494-1.202-2.172-2.129-3.676-2.024a3.183 3.183 0 00-.377.049c-.787.156-2.584.881-2.2 4.226 1.06 4.637 2.213 8.041 3.331 11.346l.023.066c.669 1.98 1.302 3.85 1.89 5.925 1.385 4.9.846 7.94.84 7.975-.046.312-.143.503-.288.57a.556.556 0 01-.195.045c-.44.03-1.098-.26-1.437-.45-.776-1.482-4.636-8.544-8.134-9.524l-.126-.037-.127.012c-1.283.121-2.226.606-2.803 1.441-.914 1.32-.535 3.002-.444 3.34l.052.12c.028.051 2.834 5.165 3.268 7.544.374 2.04 2.311 4.25 3.869 6.026l.064.073c.508.58.946 1.083 1.292 1.548 4.519 4.713 11.665 8.677 11.723 8.71.892.657 1.387 1.293 1.44 1.84a.798.798 0 01-.16.58l-.155.162.988.96 18.853-1.324.804-3.684c2.486-10.402 1.967-19.272 1.958-19.33.01-.327.706-3.483 1.266-6.033l.017-.065c1.117-5.08 2.505-11.4 2.772-13.803.116-1.028-.542-1.972-1.675-2.401z"/></svg>`;
+
+        computer.classList.add('paper-bg');
+        computer.classList.remove('rock-bg');
+        computer.classList.remove('scissors-bg');
+    }
+    if (computerRandom >= 0.66 && computerRandom < 1) {
+        computersChoice = 'scissors';
+        computerPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 50">
+        <path fill="#3B4262" d="M13.971 25.702l6.012-8.415c-2.499-.415-7.088-.507-10.846 3.235C3.212 26.421.812 39.163.312 42.248L15.37 57.24c2.711-.232 14.713-1.827 26.279-13.34.122-.249 2.94-2.321.636-4.614-1.1-1.095-2.919-1.074-4.042.044-.572.57-1.461.577-2.021.02-.56-.557-.552-1.443.02-2.012l4.087-4.069c2.076-2.067.119-5.555-2.78-4.717l-3.345 2.851c-.611.53-1.52.439-2.022-.14-.519-.597-.408-1.503.183-2.013 11.687-10.208 9.98-8.979 17.5-15.995 2.809-2.329-.725-6.447-3.493-4.09L28.182 25.45c-.529.448-1.34.457-1.86-.02-.601-.517-.615-1.262-.222-1.85L38.787 3.944c1.854-2.5-1.795-5.277-3.749-2.757L16.28 27.307c-.452.65-1.364.8-1.985.345a1.377 1.377 0 01-.323-1.95z"/></svg>`;
+
+        computer.classList.add('scissors-bg');
+        computer.classList.remove('paper-bg');
+        computer.classList.remove('rock-bg');
+    }
+
+    console.log(computersChoice);
+
+    if (computersChoice == 'scissors') {
+        resultText.innerHTML = 'Draw!';
+        player.classList.remove('show-winner');
+        computer.classList.remove('show-winner');
+    }
+    if (computersChoice == 'paper') {
+        resultText.innerHTML = 'You win!';
+        player.classList.add('show-winner');
+        computer.classList.remove('show-winner');
+        countWins();
+    }
+    if (computersChoice == 'rock') {
+        resultText.innerHTML = 'You lose!';
+        player.classList.remove('show-winner');
+        computer.classList.add('show-winner');
+    }
+}
+
+// CHOOSE PAPER
+function choosePaper() {
+    endGame.style.display = 'flex';
+    startGame.style.display = 'none';
+
+    youPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 55">
+    <path fill="#3B4262" d="M47.125 11.832a2.922 2.922 0 00-1.232-.198c-.57.04-1.029.271-1.302.65-1.604 2.248-2.919 6.493-3.979 9.905-.486 1.577-1.14 3.688-1.612 4.69-.493-2.807.064-13.09.28-17.05l.003-.064c.15-2.751.17-3.234.138-3.446-.238-1.509-.843-2.5-1.799-2.943-.966-.45-2.22-.25-3.572.563-.677.41-.865 1.816-1.446 8.19l-.002.028c-.32 3.502-1.058 11.566-1.965 12.91-1.023-1.88-2.431-12.555-3.039-17.176-.425-3.236-.673-5.094-.84-5.655-.35-1.176-1.83-2.176-3.295-2.232-1.22-.06-2.22.56-2.698 1.638-.894.995-.578 4.292.41 12.102.47 3.718 1.44 11.395.83 12.257-1.219-.133-3.31-4.942-6.215-14.299-.816-2.62-1.068-3.408-1.318-3.753-.494-1.202-2.172-2.129-3.676-2.024a3.183 3.183 0 00-.377.049c-.787.156-2.584.881-2.2 4.226 1.06 4.637 2.213 8.041 3.331 11.346l.023.066c.669 1.98 1.302 3.85 1.89 5.925 1.385 4.9.846 7.94.84 7.975-.046.312-.143.503-.288.57a.556.556 0 01-.195.045c-.44.03-1.098-.26-1.437-.45-.776-1.482-4.636-8.544-8.134-9.524l-.126-.037-.127.012c-1.283.121-2.226.606-2.803 1.441-.914 1.32-.535 3.002-.444 3.34l.052.12c.028.051 2.834 5.165 3.268 7.544.374 2.04 2.311 4.25 3.869 6.026l.064.073c.508.58.946 1.083 1.292 1.548 4.519 4.713 11.665 8.677 11.723 8.71.892.657 1.387 1.293 1.44 1.84a.798.798 0 01-.16.58l-.155.162.988.96 18.853-1.324.804-3.684c2.486-10.402 1.967-19.272 1.958-19.33.01-.327.706-3.483 1.266-6.033l.017-.065c1.117-5.08 2.505-11.4 2.772-13.803.116-1.028-.542-1.972-1.675-2.401z"/></svg>`
+
+    player.classList.add('paper-bg');
+    player.classList.remove('scissors-bg');
+    player.classList.remove('rock-bg');
+
+    let computerRandom = Math.random().toFixed(2);
+    let computersChoice = '';
+
+    if (computerRandom > 0 && computerRandom < 0.33) {
+        computersChoice = 'rock';
+        computerPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 40">
+        <path fill="#3B4262" d="M45.06 12.22c-.642-8.096-9.734-7.269-9.734-7.269-3.837-6.765-9.832-1.865-9.832-1.865-4.606-6.63-10.38-.486-10.38-.486-9.957-1.074-9.571 7.066-9.571 7.066-.234 2.588 1.403 10.593 1.403 10.593-1.477-4.614-4.68-.784-4.68-.784-3.94 6.078-.975 9.405-.975 9.405 5.33 6.246 16.688 13.743 16.688 13.743 4.113 2.356 2.373 4.457 2.373 4.457l24.876-4.11.571-4.718c3.782-11.436-.739-26.032-.739-26.032z"/></svg>`;
+
+        computer.classList.add('rock-bg');
+        computer.classList.remove('scissors-bg');
+        computer.classList.remove('paper-bg');
+    }
+    if (computerRandom >= 0.33 && computerRandom < 0.66) {
+        computersChoice = 'paper';
+        computerPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 55">
+        <path fill="#3B4262" d="M47.125 11.832a2.922 2.922 0 00-1.232-.198c-.57.04-1.029.271-1.302.65-1.604 2.248-2.919 6.493-3.979 9.905-.486 1.577-1.14 3.688-1.612 4.69-.493-2.807.064-13.09.28-17.05l.003-.064c.15-2.751.17-3.234.138-3.446-.238-1.509-.843-2.5-1.799-2.943-.966-.45-2.22-.25-3.572.563-.677.41-.865 1.816-1.446 8.19l-.002.028c-.32 3.502-1.058 11.566-1.965 12.91-1.023-1.88-2.431-12.555-3.039-17.176-.425-3.236-.673-5.094-.84-5.655-.35-1.176-1.83-2.176-3.295-2.232-1.22-.06-2.22.56-2.698 1.638-.894.995-.578 4.292.41 12.102.47 3.718 1.44 11.395.83 12.257-1.219-.133-3.31-4.942-6.215-14.299-.816-2.62-1.068-3.408-1.318-3.753-.494-1.202-2.172-2.129-3.676-2.024a3.183 3.183 0 00-.377.049c-.787.156-2.584.881-2.2 4.226 1.06 4.637 2.213 8.041 3.331 11.346l.023.066c.669 1.98 1.302 3.85 1.89 5.925 1.385 4.9.846 7.94.84 7.975-.046.312-.143.503-.288.57a.556.556 0 01-.195.045c-.44.03-1.098-.26-1.437-.45-.776-1.482-4.636-8.544-8.134-9.524l-.126-.037-.127.012c-1.283.121-2.226.606-2.803 1.441-.914 1.32-.535 3.002-.444 3.34l.052.12c.028.051 2.834 5.165 3.268 7.544.374 2.04 2.311 4.25 3.869 6.026l.064.073c.508.58.946 1.083 1.292 1.548 4.519 4.713 11.665 8.677 11.723 8.71.892.657 1.387 1.293 1.44 1.84a.798.798 0 01-.16.58l-.155.162.988.96 18.853-1.324.804-3.684c2.486-10.402 1.967-19.272 1.958-19.33.01-.327.706-3.483 1.266-6.033l.017-.065c1.117-5.08 2.505-11.4 2.772-13.803.116-1.028-.542-1.972-1.675-2.401z"/></svg>`;
+
+        computer.classList.add('paper-bg');
+        computer.classList.remove('rock-bg');
+        computer.classList.remove('scissors-bg');
+    }
+    if (computerRandom >= 0.66 && computerRandom < 1) {
+        computersChoice = 'scissors';
+        computerPicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-2 0 55 50">
+        <path fill="#3B4262" d="M13.971 25.702l6.012-8.415c-2.499-.415-7.088-.507-10.846 3.235C3.212 26.421.812 39.163.312 42.248L15.37 57.24c2.711-.232 14.713-1.827 26.279-13.34.122-.249 2.94-2.321.636-4.614-1.1-1.095-2.919-1.074-4.042.044-.572.57-1.461.577-2.021.02-.56-.557-.552-1.443.02-2.012l4.087-4.069c2.076-2.067.119-5.555-2.78-4.717l-3.345 2.851c-.611.53-1.52.439-2.022-.14-.519-.597-.408-1.503.183-2.013 11.687-10.208 9.98-8.979 17.5-15.995 2.809-2.329-.725-6.447-3.493-4.09L28.182 25.45c-.529.448-1.34.457-1.86-.02-.601-.517-.615-1.262-.222-1.85L38.787 3.944c1.854-2.5-1.795-5.277-3.749-2.757L16.28 27.307c-.452.65-1.364.8-1.985.345a1.377 1.377 0 01-.323-1.95z"/></svg>`;
+
+        computer.classList.add('scissors-bg');
+        computer.classList.remove('paper-bg');
+        computer.classList.remove('rock-bg');
+    }
+
+    console.log(computersChoice);
+
+    if (computersChoice == 'scissors') {
+        resultText.innerHTML = 'You lose!';
+        player.classList.remove('show-winner');
+        computer.classList.add('show-winner');
+    }
+    if (computersChoice == 'paper') {
+        resultText.innerHTML = 'Draw!';
+        player.classList.remove('show-winner');
+        computer.classList.remove('show-winner');
+    }
+    if (computersChoice == 'rock') {
+        resultText.innerHTML = 'You win!';
+        player.classList.add('show-winner');
+        computer.classList.remove('show-winner');
+        countWins();
+    }
+}
+
+// PLAY AGAIN
+document.getElementById('play-again').addEventListener('click', () => {
+    endGame.style.display = 'none';
+    startGame.style.display = 'block';
+});
